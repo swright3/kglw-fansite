f55fe98fdfeb6303100191117d16b89b323a6fad
Sam <samwrighthwk@gmail.com>
2022-11-28 18:24:14 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import AppCSS from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={AppCSS.App}>
+      Ayy lmao
+    </div>
+  )
+}
+
+export default App
