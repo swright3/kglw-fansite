82458bdce7bce098c671ba16b46dd50d38684922
Sam <samwrighthwk@gmail.com>
2022-12-02 17:55:25 +0000

    Splash screen animated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,20 @@
+import react, { useState } from 'react'
 import styles from './Home.module.css'
 
 function Home({id}) {
+    const [doors, setDoors] = useState('closed')
+
+    function toggleDoors() {
+        setDoors(prevDoors => prevDoors==='open' ? 'closed' : 'open')
+    }
+
     return ( 
         <div className={styles.Home}>
-            <div className={styles.splash}>
-                <div className={`${styles.leftDoor} ${styles.leftDoorOpen}`}></div>
-                <div className={`${styles.rightDoor} ${styles.rightDoorOpen}`}></div>
-            </div>
+            <div className={`${styles.leftDoor} ${doors==='open' ? styles.leftDoorOpen : ''}`}></div>
+            <div className={`${styles.rightDoor} ${doors==='open' ? styles.rightDoorOpen : ''}`}></div>
+            <button onClick={toggleDoors} className={`${styles.openBtn} ${doors==='open' ? styles.gone : ''}`}>Open the door!</button>
+            
+            <h1 className={doors==='open' ? styles.fadeIn : ''}>king gizzard and the lizard wizard</h1>
         </div>
     );
 }
