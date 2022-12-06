a3f9241431cbd28ac8446b82a78778d5da7f7a90
Sam <samwrighthwk@gmail.com>
2022-12-06 13:32:31 +0000

    Created main menu skeleton

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,21 @@ function Home({id}) {
             <div className={`${styles.rightDoor} ${doors==='open' ? styles.rightDoorOpen : ''}`}></div>
             <button onClick={toggleDoors} className={`${styles.openBtn} ${doors==='open' ? styles.gone : ''}`}>Open the door!</button>
             
-            <h1 className={doors==='open' ? styles.fadeIn : ''}>king gizzard and the lizard wizard</h1>
+            <section className={`${styles.fadeIn} ${doors==='open' ? '' : styles.invisible}`} >
+                <header className={styles.header}>
+                </header>
+                <div className={styles.menuContainer}>
+                    <div className={styles.menuItem}>
+                        <h2 className={styles.itemTitle}>Tier List</h2>
+                    </div>
+                    <div className={styles.menuItem}>
+                        <h2 className={styles.itemTitle}>Discography</h2>
+                    </div>
+                    <div className={styles.menuItem}>
+                        <h2 className={styles.itemTitle}>Socials</h2>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
